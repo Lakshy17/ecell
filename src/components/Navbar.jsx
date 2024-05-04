391e48dc6aa19751f29f45b35fba187b5fedac6a
Lakshy17 <lakshyasukruthi@gmail.com>
2024-05-04 12:09:48 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ function Navbar() {
             ele.scrollIntoView({behavior:"smooth"})
         }}
 
-    const Events = () => {
-        const ele = document.getElementById("Events");
+    const Aboutus = () => {
+        const ele = document.getElementById("Aboutus");
         if (ele) {
             ele.scrollIntoView({behavior:"smooth"})
         }}
@@ -16,7 +16,7 @@ function Navbar() {
         <>
 
  
-        <nav className="absolute flex items-center justify-center w-full bg-gradient-to-r from-green-400 to-blue-500 h-26 text-white hover:bg-black">
+        <nav className=" flex items-center justify-center w-full bg-gradient-to-r from-green-400 to-blue-500 h-26 text-white hover:bg-black fixed top-0 z-10">
             <div className="flex flex-row items-center justify-between w-full gap-10 p-6">
                 <a href="https://www.instagram.com/pes_ecell/">
                 <img src='socials.png' width={125} height={125}></img>
@@ -25,9 +25,9 @@ function Navbar() {
                 <div className="flex flex-row items-center gap-10" >
                     <p><button onClick={Home}> Home </button></p>
                     <p>|</p>
-                    <p><button onClick={Events}> Events </button></p>
+                    <p><a href='#'> Events </a></p>
                     <p>|</p>
-                    <p><a href='#'> About us </a></p>
+                    <p><button onClick={Aboutus}> About Us </button></p>
                     <p>|</p>
                     <p><a href='#'> Articles </a></p>
                     <p>|</p>
@@ -42,18 +42,22 @@ function Navbar() {
 
 
         <div className="flex flex-col items-center justify-center text-center bg-black h-screen text-white" id="Home">
-        <p className="text-5xl"> The Entrepreneurship cell </p>
-        <p className="text-1xl italic text-blue-500 mt-4"> making ideas happen </p>
-            <hr style={{ border: "none", borderTop: "1px solid white", position: "absolute", top: "53.5%", width: "55%" }} />
-            <button className="bg-white text-black font-bold py-2 px-4 rounded-full mt-10">Get Started</button>
+        <p className="text-7xl "> The Entrepreneurship cell </p>
+        <p className="text-2xl italic text-blue-500 mt-5"> making ideas happen </p>
+            <hr style={{ border: "none", borderTop: "1px solid white", position: "absolute", top: "57%", width: "55%" }} />
+            <button className="bg-white text-black py-2 px-4 rounded-full mt-20">Get Started</button>
         </div>
 
         
-        <div className="flex bg-black h-screen" id="Events">
-        
+        <div className="flex h-screen bg-black" id="Aboutus">
+    
+       
+
 
 
         </div>
+
+
 
         </>
 
